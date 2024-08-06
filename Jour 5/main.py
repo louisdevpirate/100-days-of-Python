878509ba9cfdc567b6001a093e75be7f869e4ec6
louisdevpirate <95105402+louisdevpirate@users.noreply.github.com>
2024-08-06 21:02:53 +0200

    Ajout de l'énoncé + fonction pour extraire les mots + fonction pour compter leur récurence + fonction pour afficher les mots les plus réccurents après tri + idiome de développement pour réutiliser dans un autre script

--- Jour 5/main.py
+++ Jour 5/main.py
@@ -1,13 +1,44 @@
+# Jour 5 : Créez un programme qui lit un fichier texte, compte le nombre d'occurrences de chaque mot et affiche les 5 mots les plus fréquents.
+
 import re
 
 
 def main():
     try:
-        with open('fichier.txt', 'r') as fichier:
+        with open('planning.txt', 'r') as fichier:
             content = fichier.read()
-        return content
+        words = extract_words(content)
+        words_counts = count_words(words)
+        print_top_words(words_counts)
     except FileNotFoundError:
         print("Le fichier n'a pas été trouvé.")
     except IOError:
         print("Une erreur s'est produite lors de la lecture du fichier.")
+
+
+def extract_words(content):
+    return re.findall(r'\b\w+\b', content.lower())
+
+
+def count_words(words):
+    word_counts = {}
+    for word in words:
+        if word in word_counts:
+            word_counts[word] += 1
+        else:
+            word_counts[word] = 1
+    return word_counts
+
+
+def print_top_words(words_counts):
+    sorted_words = sorted(words_counts.items(), key=lambda x: x[1], reverse=True)
+    print("Les 5 mots les plus fréquents sont:")
+    for word, count in sorted_words[:5]:
+        print(f"{word}: {count}")
+
+
+if __name__ == "__main__":
+    main()
+
+
 
