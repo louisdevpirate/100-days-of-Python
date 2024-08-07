09d35219fc63ec7c298dcf12558c5bad17ee010f
louisdevpirate <95105402+louisdevpirate@users.noreply.github.com>
2024-08-07 17:26:05 +0200

    Ajout des gestions d'erreurs

--- Jour 6 /main.py
+++ Jour 6 /main.py
@@ -17,22 +17,21 @@ def multiplication(first_number, second_number):
     result = first_number * second_number
     print(f"Le résultat est : {result}")
 
+while True:
+    operation = input("Quelle opération souhaitez-vous effectuer ? Addition/Soustraction/Multiplication/Division\nEntrez votre réponse ici : ").lower()
+    if operation in ["addition", "soustraction", "multiplication", "division"]:
+        break
+    else:
+        print("Opération non valide. Veuillez réessayer.")
 
-operation = input("Quelle opération souhaitez vous effectuer ? Addition/Soustraction/Multiplication/Division\nEntrez votre réponse ici : ")
-answer = operation.lower()
-number1 = int(input("Choisissez un premier nombre : "))
-number2 = int(input("Choisissez un deuxièpme nombre : "))
+
+def get_number(prompt):
+    while True:
+        try:
+            return float(input(prompt))
+        except ValueError:
+            print("Veuillez entrer un nombre valide.")
 
 
-if answer == "addition":
-    addition(number1, number2)
-
-if answer == "soustraction":
-    soustraction(number1, number2)
-
-if answer == "multiplication":
-    multiplication(number1, number2)
-
-if answer == "division":
-    division(number1, number2)
-
+number1 = get_number("Choisissez un premier nombre : ")
+number2 = get_number("Choisissez un deuxième nombre : ")
