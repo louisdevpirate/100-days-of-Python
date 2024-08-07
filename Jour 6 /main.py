788196c5784543050be094550eec09409e6b3c9a
louisdevpirate <95105402+louisdevpirate@users.noreply.github.com>
2024-08-07 17:40:00 +0200

    Ajout des gestions d'erreurs (valueError et divisions par zéro)

--- Jour 6 /main.py
+++ Jour 6 /main.py
@@ -8,21 +8,32 @@ def soustraction(first_number, second_number):
     print(f"Le résultat est : {result}")
 
 
-def division(first_number, second_number):
-    result = first_number / second_number
-    print(f"Le résultat est : {result}")
+def division():
+    while True:
+        try:
+            first_number = float(input("Entrez le premier nombre : "))
+            second_number = float(input("Entrez le deuxième nombre : "))
+            if second_number == 0:
+                print("Division par 0 impossible. Veuillez entrer un diviseur non nul.")
+                continue
+            result = first_number / second_number
+            print(f"Le résultat est : {result}")
+            break
+        except ValueError:
+            print("Veuillez entrer des nombres valides.")
 
 
 def multiplication(first_number, second_number):
     result = first_number * second_number
     print(f"Le résultat est : {result}")
 
+
 while True:
     operation = input("Quelle opération souhaitez-vous effectuer ? Addition/Soustraction/Multiplication/Division\nEntrez votre réponse ici : ").lower()
     if operation in ["addition", "soustraction", "multiplication", "division"]:
         break
     else:
-        print("Opération non valide. Veuillez réessayer.")
+        print("\nOpération non valide. Veuillez réessayer.\n")
 
 
 def get_number(prompt):
@@ -33,5 +44,5 @@ def get_number(prompt):
             print("Veuillez entrer un nombre valide.")
 
 
-number1 = get_number("Choisissez un premier nombre : ")
-number2 = get_number("Choisissez un deuxième nombre : ")
+number1 = get_number("\nChoisissez un premier nombre : ")
+number2 = get_number("\nChoisissez un deuxième nombre : ")
