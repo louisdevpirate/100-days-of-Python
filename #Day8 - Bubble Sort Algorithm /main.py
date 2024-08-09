af1f57d6271203ee394479488b0da151852065f8
louisdevpirate <95105402+louisdevpirate@users.noreply.github.com>
2024-08-09 22:39:48 +0200

    Optimisation du programme pour le rendre plus interactif + Gestion des erreurs

--- #Day8 - Bubble Sort Algorithm /main.py
+++ #Day8 - Bubble Sort Algorithm /main.py
@@ -1,24 +1,29 @@
 def tri_a_bulles(liste):
     n = len(liste)
     echange = True
-
-    # Continue jusqu'à ce qu'aucun échange ne soit fait
     while echange:
         echange = False
-        # Parcourt la liste jusqu'à l'avant-dernier élément
         for i in range(n - 1):
-            # Compare l'élément actuel avec le suivant
             if liste[i] > liste[i + 1]:
-                # Échange les éléments si ils sont dans le mauvais ordre
                 liste[i], liste[i + 1] = liste[i + 1], liste[i]
-                # Indique qu'un échange a eu lieu
                 echange = True
-
     return liste
 
 
-# Exemple d'utilisation
-ma_liste = [64, 34, 25, 12, 22, 11, 90]
-print("Liste non triée:", ma_liste)
+def obtenir_liste():
+    while True:
+        try:
+            entree = input("Entrez plusieurs nombres entiers, séparés par des virgules : ")
+            # Divise l'entrée en une liste de chaînes, puis convertit chaque chaîne en entier
+            ma_liste = [int(x.strip()) for x in entree.split(',')]
+            return ma_liste
+        except ValueError:
+            print("\nErreur : Veuillez entrer uniquement des nombres entiers séparés par des virgules.\n")
+
+
+# Obtenir la liste de l'utilisateur
+ma_liste = obtenir_liste()
+
+print("\nListe non triée:", ma_liste)
 liste_triee = tri_a_bulles(ma_liste)
-print("Liste triée:", liste_triee)
+print("\nListe triée:", liste_triee)
