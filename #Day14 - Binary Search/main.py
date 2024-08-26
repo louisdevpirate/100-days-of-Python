b2dd7152749bd0c473d789702344467d19e78877
louisdevpirate <95105402+louisdevpirate@users.noreply.github.com>
2024-08-26 21:56:33 +0200

    Amélioration du code de base pour le rendre plus interactif avoir une fonction d'entrée + Indication de l'index + Tri de la liste créée aléatoirement

--- #Day14 - Binary Search/main.py
+++ #Day14 - Binary Search/main.py
@@ -1,22 +1,22 @@
-# fonction recherche_binaire(liste, element_recherche):
-# debut = 0
-# fin = longueur
-# de
-# la
-# liste - 1
-#
-# tant
-# que
-# debut <= fin:
-# milieu = (debut + fin) // 2
-# if liste[milieu] == element_recherche:
-#     return milieu  # Élément trouvé
-# elif liste[milieu] < element_recherche:
-#     debut = milieu + 1
-# else:
-#     fin = milieu - 1
-#
-# return -1  # Élément non trouvé
+import random
+
+
+def create_list():
+    random_list = []
+    for i in range(20):
+        random_list.append(random.randint(0, 100))
+    return random_list
+
+
+def number_to_search():
+    while True:
+        try:
+            nombre_a_chercher = int(input("Veuillez entrer un nombre (entier) à chercher dans la liste : "))
+            return nombre_a_chercher
+        except ValueError:
+            print("Non c'est pas bon, ce n'est pas un nombre entier")
+            continue
+
 
 def binary_search(list, input):
     start = 0
@@ -25,18 +25,24 @@ def binary_search(list, input):
     while start <= end:
         middle = (start + end) // 2
         if list[middle] == input:
-            print("Trouvé !")
             return middle
         elif list[middle] < input:
             start = middle + 1
         else:
             end = middle - 1
 
-    print("Nombre introuvable")
     return -1
 
 
-liste = [2, 4, 7, 9, 10, 12, 13, 14, 15, 18, 56]
-nombre_a_chercher = 16
+if __name__ == "__main__":
+    liste = create_list()
+    liste.sort()  # On trie la liste
+    print("Liste générée et triée :", liste)
 
-binary_search(liste, nombre_a_chercher)
+    nombre = number_to_search()
+    resultat = binary_search(liste, nombre)
+
+    if resultat != -1:
+        print(f"Le nombre {nombre} a été trouvé à l'index {resultat}")
+    else:
+        print(f"Le nombre {nombre} n'est pas dans la liste")
